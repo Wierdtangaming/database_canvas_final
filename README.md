974ccdf536493d9e7a231ae48da9afcce64387aa
Wierdcreations <33354033+Wierdtangaming@users.noreply.github.com>
2020-03-03 20:03:04 +0530

    Universal canvas
    Old drawings load and new drawings real time update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# database_canvas_final
+# Universal canvas
+
+Log 1 : 6:30
+
+Firebase Init and drawing with vertex
+
+
+Log 2 : 8:00
+
+Old drawings load and new drawings real time update
---
+++ sketch.js
@@ -0,0 +1,86 @@
+var drawing = [];
+var currentPath = [];
+var savebutton;
+var isDrawing = false;
+var old_drawing = [];
+function setup() {
+    database = firebase.database();
+    dh = displayHeight / 3;
+    dw = displayWidth / 30;
+
+
+    canvas = createCanvas(displayWidth - dw,displayHeight - dh);
+    canvas.mousePressed(startPath);
+    canvas.mouseReleased(endPath);
+    canvas.parent('canvasContainer');
+
+    bg = prompt("Enter bg colour");
+    clr = prompt("Enter pen colour");
+    console.log(bg+clr);
+    if (bg = 0) {
+        bg = "black";
+    }
+    if (clr = 0) {
+        clr = "white";
+    }
+    var drawing_done = database.ref('drawings/drawing')
+    drawing_done.once("value",drawn,showError);
+   
+}
+
+function draw(){
+    background(bg);
+    if(isDrawing === true){
+        var point = {
+            x : mouseX,
+            y : mouseY 
+        }
+        currentPath.push(point);
+    }
+
+    stroke(clr);
+    noFill();
+
+    for (var i = 0; i < drawing.length;i++) {
+        var path = drawing[i];
+        beginShape();
+        for (var a = 0; a < path.length;a++) {
+            vertex(path[a].x,path[a].y);
+        }
+        endShape();
+    }
+    for (var i = 0; i < old_drawing.length;i++) {
+        var path1 = old_drawing[i];
+        beginShape();
+        for (var a = 0; a < path1.length;a++) {
+            vertex(path1[a].x,path1[a].y);
+        }
+        endShape();
+    }
+
+}
+
+function startPath() {
+    isDrawing = true;
+    currentPath = [];
+    drawing.push(currentPath);
+}
+
+
+function endPath() {
+    var ref = database.ref('drawings').set({
+        name : "TD",
+        drawing : drawing
+    });
+    isDrawing = false;
+}
+
+function showError(){
+    console.log("Error");
+    
+}
+
+function drawn(data) {
+    old_drawing  = data.val();
+    console.log(old_drawing);
+}
